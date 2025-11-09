c97bd5556a00b6ab8d095bdf1f067a6fc886c43d
FahimTasnimKhan <fahimtasnimkhan@gmail.com>
2025-11-09 19:00:33 +0600

    Initial Server Structure and MongoDB Connnected Successfully

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './src/config/db.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Habitude App API');
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
